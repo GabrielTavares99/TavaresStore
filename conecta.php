79b119dee3260e9900f103448d9540abf3280eee
GabrielTavares99 <gabriel.tavares99@hotmail.com>
2017-05-20 14:31:08 -0300

    Arrumando classe de conexão

--- conecta.php
+++ conecta.php
@@ -1,3 +1,4 @@
 <?php 
 
-	$conexao = mysqli_connect('localhost','root','','bd_loja');
+	//$conexao = mysqli_connect("https:mysql.hostinger.com.br","u923324639_loja", "lojaloja123",'u923324639_loja');
+	$conexao = mysqli_connect("localhost","root","","bd_loja");
